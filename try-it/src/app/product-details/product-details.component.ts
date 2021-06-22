266845eeb8aa73364be3ce3ae41129f3bf98c251
SauzeauYannis <sauzeauyannis@gmail.com>
2021-06-22 12:32:00 +0200

    Continue the 'Try it' Angular project

---
+++ try-it/src/app/product-details/product-details.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {Product, products} from "../products";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+  ) {
+  }
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+}
--- try-it/src/app/product-list/product-list.component.ts
+++ try-it/src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { products } from '../products';
+import {products} from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -12,6 +12,10 @@ export class ProductListComponent {
 
   share() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
