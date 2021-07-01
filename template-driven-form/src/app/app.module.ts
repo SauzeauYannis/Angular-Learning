d50f30b21f38ab9ab846fba79360bb14c9fbb292
Yannis <sauzeauyannis@gmail.com>
2021-07-01 12:05:18 +0200

    Add a form website

---
+++ template-driven-form/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {HeroFormComponent} from './hero-form/hero-form.component';
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
